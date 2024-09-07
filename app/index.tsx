2e38c5c2be9307d19d1438e4069a19c97c00ba66
TriiNguyen <nguyeminhtrii@gmail.com>
2024-09-07 16:45:15 +0700

    fix sth

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,5 @@
-import { View, Text } from "react-native";
-import React from "react";
 import { Link } from "expo-router";
 
-export default function Home() {
+export default function Index() {
   return <Link href="/login">Go to Login</Link>;
 }
